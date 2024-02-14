bb5e42a429b0ec1306c44d829f866ce7487039f5
jckr0814 <141427015+jckr0814@users.noreply.github.com>
2024-02-14 21:51:12 +0800

    Database Table and Data Script

---
+++ dbo.userTable.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[userTable] ON
+INSERT INTO [dbo].[userTable] ([Id], [fname], [lname], [gender], [interestedTopic], [email], [username], [password], [usertype]) VALUES (1, N'mem1', N'ber1', N'M         ', N'c', N'member1@gmail.com', N'member1', N'member1', N'member    ')
+INSERT INTO [dbo].[userTable] ([Id], [fname], [lname], [gender], [interestedTopic], [email], [username], [password], [usertype]) VALUES (2, N'mem2', N'ber2', N'F         ', N'b', N'member2@gmail.com', N'member2', N'member2', N'member    ')
+SET IDENTITY_INSERT [dbo].[userTable] OFF
---
+++ dbo.userTable.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[userTable] (
+    [Id]              INT           IDENTITY (1, 1) NOT NULL,
+    [fname]           NVARCHAR (50) NULL,
+    [lname]           NVARCHAR (50) NULL,
+    [gender]          NCHAR (10)    NULL,
+    [interestedTopic] NVARCHAR (50) NULL,
+    [email]           NVARCHAR (50) NULL,
+    [username]        NVARCHAR (50) NULL,
+    [password]        NVARCHAR (50) NULL,
+    [usertype]        NCHAR (10)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

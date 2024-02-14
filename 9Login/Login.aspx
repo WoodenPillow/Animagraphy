0fe4c65df95869ff45d61d6bb8d2246d9acb0776
JOSHUA CHONG KAI REN <TP068851@mail.apu.edu.my>
2024-02-14 17:11:05 +0800

    Login Page

---
+++ 9Login/Login.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WAPP_Assignment.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head runat="server">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Welcome to Animagraphy Login Page</title>
+    <link rel= "stylesheet" href="login.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+</head>
+
+<body>
+    <div class="wrapper">
+        <form id="form1" runat="server">
+            <h1>Login</h1>
+                <div class="input-box">
+                    <input type="text" id="login_username" placeholder="Username" required="required" runat="server"/>
+                    <box-icon type='solid' name='user'></box-icon>
+                </div>
+                <div class="input-box">
+                    <input type="password" id="login_pwd" placeholder="Password" required="required" runat="server"/>
+                    <box-icon name='lock-alt' ></box-icon>
+                </div>
+            <div class="forgot-password">
+                <a href="#">Forgot password?</a>
+            </div>
+            <button id="Button1" type="submit" class="btn" form="form1"  runat="server">Login</button>
+            <div class="register-link">
+                <p>Don't have an account? <a href="Registration.aspx">Register</a></p>
+            </div>
+            <asp:Label style="position:center" ID="prompt" runat="server" Visible="false"></asp:Label>
+            </form>
+        </div>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:assignmentDatabase %>" SelectCommand="SELECT * FROM [ass_database]"></asp:SqlDataSource>
+</body>
+</html>
---
+++ 9Login/Login.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace WAPP_Assignment
+{
+    public partial class Login : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["assignmentDatabase"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM [dbo].[ass_database] WHERE username = @username AND Password = @password";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@username", login_username.Value);
+                cmd.Parameters.AddWithValue("@password", login_pwd.Value);
+
+                try
+                {
+                    con.Open();
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        query = "SELECT [First Name], [Usertype] FROM [dbo].[ass_database] WHERE username = @username";
+                        cmd.CommandText = query;
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        string type = "";
+                        string name = "";
+
+                        while (dr.Read())
+                        {
+                            type = dr["usertype"].ToString().Trim();
+                            name = dr["First Name"].ToString().Trim();
+                        }
+
+                        Session["fname"] = name;
+
+                        if (type == "admin")
+                        {
+                            Response.Redirect("adminDashboard.aspx");
+                        }
+                        else if (type == "member")
+                        {
+                            Response.Redirect("memberDashboard.aspx");
+                        }
+                        else
+                        {
+                            prompt.Visible = true;
+                            prompt.ForeColor = System.Drawing.Color.Red;
+                            prompt.Text = "Username and Password Mismatch!";
+                        }
+                    }
+                    else
+                    {
+                        prompt.Visible = true;
+                        prompt.ForeColor = System.Drawing.Color.Red;
+                        prompt.Text = "Invalid username or password!";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions (e.g., logging, displaying error message)
+                    prompt.Visible = true;
+                    prompt.ForeColor = System.Drawing.Color.Red;
+                    prompt.Text = "An error occurred: " + ex.Message;
+                }
+            }
+        }
+    }
+}

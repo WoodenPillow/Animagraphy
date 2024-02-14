6c5fdc92104c30ac1bb573e9f852665d1a2a719a
JOSHUA CHONG KAI REN <TP068851@mail.apu.edu.my>
2024-02-14 16:47:58 +0800

    Registration Page

---
+++ 10Registration/Registration.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="WAPP_Assignment.Registration" EnableViewState="true"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head runat="server">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Welcome to Animagraphy Registration Page</title>
+    <link rel="stylesheet" href="registration.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+    <style>
+        .form-group {
+            margin-bottom: 10px;
+        }
+        .error-message {
+            color: red; /* Set error message color to red */
+            margin-top: 5px;
+        }
+         #gender {
+             font-size: 13px;
+            text-align:center;
+            background-color: #333;
+         }
+         #interestedTopic {
+             font-size: 13px;
+             text-align: center;
+             background-color: #333;
+         }
+    </style>
+</head>
+<body>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:assignmentDatabase %>" SelectCommand="SELECT * FROM [ass_database]"></asp:SqlDataSource>
+    <div class="wrapper">
+        <form id="form1" runat="server" enctype="multipart/form-data" enableviewstate="true">
+            <h1>Animagraphy Registration Page</h1>
+                <div class="input-box">
+                    <div class="input-field"><asp:Textbox id="fname" placeholder="First Name" 
+                        required="required" runat="server"/>
+                    </div>
+                    <div class="input-field"><asp:Textbox id="lname" placeholder="Last Name" 
+                        required="required" runat="server"/>
+                    </div>
+                    <div class="input-field">
+                        <asp:DropDownList ID="gender" required="required" runat="server">
+                            <asp:ListItem Value="" Text="-- Select Gender --"></asp:ListItem>
+                            <asp:ListItem Value="M">Male</asp:ListItem>
+                            <asp:ListItem Value="F">Female</asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+                    <div class="input-field">
+                        <asp:DropDownList ID="interestedTopic" required="required" runat="server">
+                            <asp:ListItem Value="" Text="-- Select Interested Topic --"></asp:ListItem>
+                            <asp:ListItem Value="a">A</asp:ListItem>
+                            <asp:ListItem Value="b">B</asp:ListItem>
+                            <asp:ListItem Value="c">C</asp:ListItem>
+                            <asp:ListItem Value="d">D</asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+
+                    <div class="input-field"><asp:Textbox id="email" placeholder="Email"
+                        required="required" runat="server"/>
+                    </div>
+                    <div class="input-field"><asp:Textbox id="username" placeholder="Username" 
+                        required="required" runat="server"/>
+                    </div>
+                    <div class="input-field"><asp:Textbox id="pwd" placeholder="Password" TextMode="Password"
+                        required="required" runat="server"/>
+                    </div>                    
+                </div>
+            <input type="checkbox" id="chkAgreement" required="required" />
+            <label for="chkAgreement">I hereby declare that the above information provided is true and correct.</label>
+            <button type="submit" class="btn" id="Button1">Register</button>
+            <asp:Label ID="prompt" runat="server" CssClass="error-message" Visible="false"></asp:Label>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ 10Registration/Registration.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace WAPP_Assignment
+{
+    public partial class Registration : System.Web.UI.Page
+        {
+        protected void Button1_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                // Log a message indicating that the button click event handler is being executed
+                System.Diagnostics.Debug.WriteLine("Register button clicked. Processing registration...");
+          
+                string connectionString = ConfigurationManager.ConnectionStrings["assignmentDatabase"].ConnectionString;
+
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+
+                        string username = this.username.Text.Trim();
+                        if (string.IsNullOrEmpty(username))
+                        {
+                            throw new Exception("Username cannot be empty.");
+                        }
+
+                        string query = "SELECT COUNT(*) FROM [dbo].[ass_database] WHERE username = @username";
+                        using (SqlCommand cmd = new SqlCommand(query, con))
+                        {
+                            cmd.Parameters.AddWithValue("@username", username);
+                            int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                            if (count > 0)
+                            {
+                                prompt.Visible = true;
+                                prompt.ForeColor = System.Drawing.Color.Red;
+                                prompt.Text = "Username has been taken!";
+                            }
+                            else
+                            {
+                                string insertQuery = "INSERT INTO [dbo].[ass_database] ([First Name], [Last Name], Gender, [Interested Topic], Email, Username, Password) VALUES (@firstName, @lastName, @gender, @interestedTopic, @email, @username, @password)";
+                                using (SqlCommand insertCmd = new SqlCommand(insertQuery, con))
+                                {
+                                    insertCmd.Parameters.AddWithValue("@firstName", fname.Text);
+                                    insertCmd.Parameters.AddWithValue("@lastName", lname.Text);
+                                    insertCmd.Parameters.AddWithValue("@gender", gender.SelectedValue);
+                                    insertCmd.Parameters.AddWithValue("@interestedTopic", interestedTopic.SelectedValue);
+                                    insertCmd.Parameters.AddWithValue("@email", email.Text);
+                                    insertCmd.Parameters.AddWithValue("@username", username);
+                                    insertCmd.Parameters.AddWithValue("@password", HashPassword(pwd.Text));
+                                    insertCmd.ExecuteNonQuery();
+                                }
+                
+                            // Ensure that the redirection to the login page is attempted
+                            System.Diagnostics.Debug.WriteLine("Redirecting to Login.aspx...");
+                            Response.Redirect("Login.aspx");
+                             }
+                         }
+                     }
+                }
+                catch (Exception ex)
+                {
+                // Log any exceptions that occur during the registration process
+                System.Diagnostics.Debug.WriteLine("Error occurred during registration: " + ex.Message);
+
+                // Display an error message to the user
+                prompt.Visible = true;
+                prompt.ForeColor = System.Drawing.Color.Red;
+                prompt.Text = "Registration failed: " + ex.Message;
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            byte[] salt = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

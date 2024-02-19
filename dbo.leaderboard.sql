8578bbedb8c4c4ca1e84fb0384649209a248f53f
jckr0814 <141427015+jckr0814@users.noreply.github.com>
2024-02-19 21:25:44 +0800

    Table Structure and Data Update

---
+++ dbo.leaderboard.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[leaderboard] (
+    [Id]            INT IDENTITY(1,1) NOT NULL,
+    [name]          NVARCHAR (50) NULL,
+    [points]        FLOAT (53)    NULL,
+    [learningHours] FLOAT (53)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
